9872e6999697332f2040353642c530396054ec29
叶新健 <xinjian0728@hotmail.com>
2016-02-02 00:08:05 +0800

    新增记录和测试

--- start/src/test/java/start/HelloTest.java
+++ start/src/test/java/start/HelloTest.java
@@ -1,8 +1,14 @@
 package start;
 
+import java.io.File;
+
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 
 import com.yexj.learn.spring.start.Hello;
 
@@ -13,5 +19,20 @@ public class HelloTest {
 		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 		Hello hello = (Hello)context.getBean("hello");
 		hello.sayHello("yexj");
+		// 测试使用类名来从容器中获取
+		Hello h = (Hello)context.getBean(Hello.class);
+		h.sayHello("ye");
 	}
+	
+	// 加载资源的方式
+	@Test
+	public void test2(){
+		File file = new File("src/main/resources/application.xml");
+		Resource resource = new FileSystemResource(file);
+		BeanFactory factory = new XmlBeanFactory(resource);
+		Hello hello = (Hello)factory.getBean("hello");
+		hello.sayHello("小叶");
+	}
+	
+	
 }
